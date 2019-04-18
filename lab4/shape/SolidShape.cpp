fab760766a2408c52795701f5ed3f8887b9268ad
Artemii <chekanovartemii99@mail.ru>
2019-04-18 09:17:58 +0300

    lab4

---
+++ lab4/shape/SolidShape.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "SolidShape.h"
+
+std::string CSolidShape::GetFillColor() const
+{
+	return m_fillColor;
+}
+
+void CSolidShape::SetFillColor(const std::string& color)
+{
+	m_fillColor = color;
+}
---
+++ lab4/shape/SolidShape.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Shape.h"
+
+class CSolidShape : public CShape
+{
+public:
+	std::string GetFillColor() const;
+	void SetFillColor(const std::string& color);
+
+private:
+	std::string m_fillColor;
+};
